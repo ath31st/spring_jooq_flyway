4c3383034deaca7e6d7c340262eb6268beb90d0b
Vladimir Olennikov <ath31st@yandex.ru>
2023-10-15 15:30:10 +0300

    update application.yaml, deleted column population for houses

--- src/main/resources/db/migration/V003__create_table_houses.sql
+++ src/main/resources/db/migration/V003__create_table_houses.sql
@@ -3,7 +3,6 @@ CREATE TABLE houses
     id            INTEGER PRIMARY KEY,
     house_number  VARCHAR(10) NOT NULL,
     date_of_build DATE,
-    population    INTEGER,
     street_id     INTEGER,
     FOREIGN KEY (street_id) REFERENCES streets (id)
 );
