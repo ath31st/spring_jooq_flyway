837a0909429fe23f8dc3237973d67486f7523ce8
Vladimir Olennikov <ath31st@yandex.ru>
2023-10-15 14:00:00 +0300

    init project

---
+++ src/main/java/sidim/doma/sjf/SpringJooqFlywayApplication.java
@@ -0,0 +1,13 @@
+package sidim.doma.sjf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJooqFlywayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringJooqFlywayApplication.class, args);
+  }
+
+}
---
+++ src/test/java/sidim/doma/sjf/SpringJooqFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package sidim.doma.sjf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJooqFlywayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
