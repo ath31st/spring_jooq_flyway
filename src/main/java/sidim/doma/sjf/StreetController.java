72d69fbe21af646cd3cb30bf3f27139d3fe7a108
Vladimir Olennikov <ath31st@yandex.ru>
2023-10-18 20:20:13 +0300

    added methods for street controller

--- src/main/java/sidim/doma/sjf/StreetController.java
+++ src/main/java/sidim/doma/sjf/StreetController.java
@@ -1,10 +1,28 @@
 package sidim.doma.sjf;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sidim.doma.sjf.db.tables.pojos.City;
 
 @RestController
 @RequiredArgsConstructor
 public class StreetController {
+  private final StreetService streetService;
 
+  @GetMapping("/street/{name}")
+  ResponseEntity<City> getStreet(@PathVariable String name) {
+    return ResponseEntity.ok(streetService.findByName(name));
+  }
+
+  @PostMapping("/street/new")
+  ResponseEntity<HttpStatus> insertStreet(@RequestBody StreetDto dto) {
+    streetService.insertStreet(dto);
+    return ResponseEntity.ok(HttpStatus.CREATED);
+  }
 }
