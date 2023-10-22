dcd92aed8c66c4b12fccf44e8652ef3d27297183
Vladimir Olennikov <ath31st@yandex.ru>
2023-10-22 22:55:54 +0300

    added house service

---
+++ src/main/java/sidim/doma/sjf/service/HouseService.java
@@ -0,0 +1,31 @@
+package sidim.doma.sjf.service;
+
+import static sidim.doma.sjf.db.Tables.HOUSE;
+
+import lombok.RequiredArgsConstructor;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Service;
+import sidim.doma.sjf.dto.HouseDto;
+import sidim.doma.sjf.model.Street;
+
+@Service
+@RequiredArgsConstructor
+public class HouseService {
+  private final DSLContext dsl;
+  private final HouseRecordMapper mapper;
+
+  public void insertHouse(HouseDto dto) {
+    dsl.insertInto(HOUSE)
+        .columns(HOUSE.HOUSE_NUMBER, HOUSE.DATE_OF_BUILD, HOUSE.STREET_ID)
+        .values(dto.houseNumber(), dto.dateOfBuild(), dto.streetId())
+        .execute();
+  }
+
+  public Street findByNumber(Integer houseNumber) {
+    return dsl.select(HOUSE)
+        .from(HOUSE)
+        .where(HOUSE.HOUSE_NUMBER.eq(houseNumber))
+        .fetchOptional(mapper)
+        .orElseThrow();
+  }
+}
